057407330205d206cd29a4d343fc280e00629ce5
bdastur <bdastur@gmail.com>
2016-06-01 04:08:58 -0700

    Move common functions to aws_iam_operations.

--- scripts/aws_add_user_operation.sh
+++ scripts/aws_add_user_operation.sh
@@ -18,6 +18,7 @@ create_logincredentials=false
 delimiter=false
 force_password=
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ####################################################
 # show_help: 
@@ -69,6 +70,10 @@ function debug()
         fi
     fi
 }
+
+# Source helper scripts.
+source $DIR/aws_iam_operations.sh
+
 
 function validate_input()
 {
@@ -138,181 +143,6 @@ function validate_input()
     fi
     debug ": All policies Valid"
     echo "Validations.. Complete"
-}
-
-function create_user()
-{
-    tmpfile="/tmp/ucreate.tmp"
-
-    python - << END
-import boto3 
-import botocore
-
-
-session = boto3.Session(profile_name="${account}")
-iamclient = session.client('iam')
-try:
-    iamuser = iamclient.create_user(UserName="$user")
-except botocore.exceptions.ClientError as boto_exception:
-    print "[%s] " % boto_exception
-    fp = open("${tmpfile}", 'w')
-    fp.close()
-
-END
-
-    if [[ -e $tmpfile ]]; then
-        echo "Error: Failed to create user $user"
-        rm $tmpfile
-        exit 1
-    else
-        debug "User $user created."
-    fi
-}
-
-function add_user_to_group()
-{
-    tmpfile="/tmp/gadderr.tmp"
-
-    python - << END
-import boto3 
-import botocore
-
-
-session = boto3.Session(profile_name="${account}")
-iamclient = session.client('iam')
-try:
-    iamuser = iamclient.add_user_to_group(UserName="$user", GroupName="$group")
-except botocore.exceptions.ClientError as boto_exception:
-    print "[%s] " % boto_exception
-    fp = open("${tmpfile}", 'w')
-    fp.close()
-
-END
-
-    if [[ -e $tmpfile ]]; then
-        echo "Error: Failed to Add user $user to group $group"
-        rm $tmpfile
-        exit 1
-    else
-        debug "User $user added to group $group"
-    fi
-
-}
-
-function attach_user_policies()
-{
-    if [[ -z $policies ]]; then
-        echo "No User policies to attach."
-        return
-    fi
-
-    tmpfile="/tmp/attachpolicyerr.tmp"
-
-    if [[ $delimiter == true ]]; then
-        OFS=$IFS
-        IFS=":"
-    fi
-
-    policyarr=($policies)
-    for policy in "${policyarr[@]}"
-    do
-        policy_arn=$(aws iam list-policies --profile $account | grep $policy | awk -F" " '{print $2}')
-
-    python - << END
-import boto3 
-import botocore
-
-session = boto3.Session(profile_name="${account}")
-iamclient = session.client('iam')
-try:
-    iamclient.attach_user_policy(UserName="$user", PolicyArn="$policy_arn")
-except botocore.exceptions.ClientError as boto_exception:
-    print "[%s] " % boto_exception
-    fp = open("${tmpfile}", 'w')
-    fp.close()
-
-END
-    done
-
-    if [[ $delimiter == true ]]; then
-        IFS=$OFS
-    fi
-
-    if [[ -e $tmpfile ]]; then
-        echo "Error: Failed to attach policies to $user"
-        rm $tmpfile
-        exit 1
-    else
-        debug "Policies $policies attached to User $user"
-    fi
-}
-
-function create_access_key()
-{
-    accesskey_loc="/tmp/${user}_accesskey"
-    if [[ $create_accesskey == true ]]; then
-        accesskey=$(aws iam create-access-key --user-name  $user --profile $account --output json)
-        echo $accesskey >  $accesskey_loc
-    fi
-}
-
-function create_login_credentials()
-{
-
-   tmpfile="/tmp/loginprofilerr.tmp"
-
-   if [[ $create_logincredentials == true ]]; then
-       if [[ ! -z $force_password ]]; then
-           password_flag="True"
-       else
-           password_flag="False"
-       fi 
-
-    python - << END
-import random
-import string
-import boto3
-import botocore
-
-
-size = 10
-chars = string.digits + string.ascii_letters + "[{}]"
-
-if "$password_flag" == "True":
-    user_password = "$force_password"
-    reset_required = False 
-else:
-    user_password = ''.join(random.choice(chars) for _ in range(size))
-    user_password = user_password + "{"
-    reset_required = True
-
-print "Userpass: ", user_password
-logincreds_file = "/tmp/$user" + "_logincredentials"
-
-session = boto3.Session(profile_name="${account}")
-iamclient = session.client('iam')
-try:
-    login_profile = iamclient.create_login_profile(UserName="$user", Password=user_password, PasswordResetRequired=reset_required)
-    fp = open(logincreds_file, "w")
-    fp.write(str(login_profile))
-    fp.write(user_password)
-    fp.close()
-except botocore.exceptions.ClientError as boto_exception:
-    print "[%s] " % boto_exception
-    fp = open("${tmpfile}", 'w')
-    fp.close()
-
-END
-
-       if [[ -e $tmpfile ]]; then
-           echo "Failed to create login profile for $user"
-           rm $tmpfile
-           exit 1 
-       else
-           debug "Login profile for $user created"
-       fi 
-
-   fi 
 }
 
 
